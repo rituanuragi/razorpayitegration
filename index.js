4e30993c305f81dfde0a783ad2df7de780ac1f24
Ritu Anuragi <81502161+rituanuragi@users.noreply.github.com>
2024-01-23 10:26:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const paymentRoutes = require("./Routes/payment");
+const app = express();  // Create an instance of the Express application
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/payment/",paymentRoutes);
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}`));
